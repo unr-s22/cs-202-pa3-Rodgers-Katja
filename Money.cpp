90401533a1035429e58fcebebbe8591a871466a4
finnenicholas <nfinne@nevada.urn.edu>
2022-03-11 12:25:02 -0800

    v0 initial files and code he gave us being setup

--- Money.cpp
+++ Money.cpp
@@ -1,1 +1,17 @@
 #include "Money.h"
+
+/*
+CODE HE GAVE US:
+
+bool operator<(const Money &rhs) const {
+	return all_cents < rhs.all_cents;
+}
+
+friend std::ostream &operator<<(std::ostream &os, const Money & money){
+	os << "$" << money.all_cents * 0.01;
+	return os;
+}
+
+Money m;
+auto sum_deposits = std::accumulate(deposits.begin(), deposits.end(), m);
+*/
