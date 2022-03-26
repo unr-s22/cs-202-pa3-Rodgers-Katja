7043d3afc9db235fe7b8ceee8f185a6f48d9235c
finnenicholas <nfinne@nevada.urn.edu>
2022-03-25 17:53:55 -0700

    Added prototype of main

--- main.cpp
+++ main.cpp
@@ -32,5 +32,40 @@ int main(){
 	cout << (m + m1) << endl;
 	cout << (m - m1) << endl;
 
+	/*
+	===========
+	Actual Main
+	===========
+	Money money(300, 23);
+	Account account(money);
+	//D means deposit, W means withdrawl, C means check
+	Money d1(200, 0), d2(300, 24), d3(501, 22), w1(300, 10), w2(201, 34), c1(1301, 69), c2(800, 25);
+
+	cout << account << endl;
+	//numbers will probably change when I see account class
+	account.makeDeposit(d1);
+	account.makeDeposit(d2);
+	account.makeDeposit(d3);
+
+	cout << account << endl;
+
+	if(account.Money == c1){
+		cout << ***Check 1 was successful*** << endl;
+	} else {
+		cout << ***Check 1 failed*** << endl;
+	}
+
+	account.makeWithdrawals(w1);
+	account.makeWithdrawals(w2);
+
+	cout << account << endl;
+
+	if(account.Money == c2){
+		cout << ***Check 2 was successful*** << endl;
+	} else {
+		cout << ***Check 2 failed*** << endl;
+	}
+	*/
+	
 	return 0;
 }
