3adefd5728327e3cdd17845ecf63f9e82d505341
finnenicholas <nfinne@nevada.urn.edu>
2022-03-30 23:05:16 -0700

    proving that operational relations work

--- main.cpp
+++ main.cpp
@@ -14,6 +14,17 @@ int main(){
 
 	cout << account << endl;
 
+	cout << "==================================" << endl;
+	cout << "Proving relational operations work" << endl;
+	cout << "==================================" << endl << endl;
+
+	if(d1 < d2){cout << "d1(" << d1 << ") is less than d2(" << d2 << ")." << endl;}
+	if(d3 > d2){cout << "d3(" << d3 << ") is greater than d2(" << d2 << ")." << endl;}
+	if(d1 <= d2){cout << "d1(" << d3 << ") is less than or equal to d2(" << d2 << ")." << endl;}
+	if(d2 >= d2){cout << "d2(" << d2 << ") is greater than or equal to d2(" << d2 << ")." << endl;}
+	if(d1 != d3){cout << "d1(" << d1 << ") does not equal to d3(" << d3 << ")." << endl;}
+	if(d2 == d2){cout << "d2(" << d2 << ") is equal to d2(" << d2 << ")." << endl << endl;}
+
 	account.makeDeposit(d1);
 	account.makeDeposit(d2);
 	account.makeDeposit(d3);
